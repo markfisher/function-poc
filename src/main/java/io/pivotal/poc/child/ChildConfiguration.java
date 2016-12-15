1a3bd7e3a8bbf56329a62bb6a9ca4e19d3645b93
markfisher <mfisher@pivotal.io>
2016-12-15 14:12:16 -0500

    initial commit

---
+++ src/main/java/io/pivotal/poc/child/ChildConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.poc.child;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+
+import reactor.core.publisher.Flux;
+
+@EnableBinding(Processor.class)
+public class ChildConfiguration {
+
+	@StreamListener
+	@Output(Processor.OUTPUT)
+	public Flux<String> process(@Input(Processor.INPUT) Flux<String> input) {
+		return input.map(s -> s.toUpperCase());
+	}
+}
